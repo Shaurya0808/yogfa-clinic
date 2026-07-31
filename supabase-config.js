3694b1c11f91b8ac49b971d949d8fae2ea8a616e
Shaurya Negi <shauryaneginegi29@gmail.com>
2026-07-31 22:40:34 +0530

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,6 +0,0 @@
-const { createClient } = supabase;
-
-const SUPABASE_URL = 'https://ymoggzekdxjkobljpxud.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_37up80ESumtXyVvmSjCrUg_KONeIGYX';
-
-const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
